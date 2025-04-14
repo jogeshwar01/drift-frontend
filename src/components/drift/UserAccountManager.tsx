e950ecdbfcbd466e0cb71289768b56f0db7b5ed0
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2025-04-15 01:07:14 +0530

    setup: shadcn in nextjs

--- src/components/drift/UserAccountManager.tsx
+++ src/components/drift/UserAccountManager.tsx
@@ -189,7 +189,7 @@ export function UserAccountManager() {
       )}
 
       {!isLoadingAccounts && userAccounts.length === 0 && (
-        <div className="text-center py-8 h-[60vh]">
+        <div className="text-center py-8 h-[60vh] flex justify-center items-center">
           <p className="text-gray-300">
             No accounts found. Create a new account to get started.
           </p>
