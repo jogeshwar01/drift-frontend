77ff3fa240f3e4c3e4b451dbc4b19f41bf9183bf
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2025-04-16 00:15:44 +0530

    style: update drift favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Drift Frontend",
   description: "Drift Frontend",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
